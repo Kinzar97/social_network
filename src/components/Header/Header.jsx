5092a5434bcc5b128df4a237d5e115a9bcf23171
Kinzar97 <igor.zaripov.igor@gmail.com>
2020-04-07 01:29:10 +0300

    changed structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from './Header.module.css';
 
 const Header = ()=>{
 return <header className={s.header}>
-        <img src= {require('../images/main_logo.png')} alt = "Logo"/>
+        <img src= {require('../../images/main_logo.png')} alt = "Logo"/>
 </header>
 };
 
