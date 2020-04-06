0a0a6711765fce069cdb5cf88b1dfdab3103bbd2
Kinzar97 <igor.zaripov.igor@gmail.com>
2020-04-06 05:19:15 +0300

    Сделал лого!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Header = ()=>{
 return <header className="header">
-        <img src='https://c7.hotpng.com/preview/18/497/108/react-javascript-responsive-web-design-github-angularjs-github.jpg'>
-        </img>
+        <img src= {require('../images/main_logo.png')} alt = "Logo"/>
 </header>
 };
 
