13934cd923bd957e9d48b9aa1056c30268954226
Kinzar97 <igor.zaripov.igor@gmail.com>
2020-04-08 04:00:50 +0300

    changed photo and description

--- src/components/Profile/Photos/Photos.jsx
+++ src/components/Profile/Photos/Photos.jsx
@@ -3,7 +3,7 @@ import s from './Photos.module.css'
 
 const Photos = () =>{
 return <div className = {s.photos}>
-      <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"  height = "250px" alt = "photo profile"></img>
+      <img src="https://sun9-61.userapi.com/c837137/v837137813/53b53/L7Rq-oHPtYI.jpg"  height = "250px" alt = "photo profile"></img>
     </div>
 };
 export default Photos;
