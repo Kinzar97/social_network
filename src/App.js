05b32c6b74162bca41ffaefd17823530c9210567
Kinzar97 <igor.zaripov.igor@gmail.com>
2020-04-10 04:06:30 +0300

    added content in message companent

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import s from './App.module.css';
+import './App.css';
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
 import Profile from './components/Profile/Profile';
+import Messages from './components/Messages/Messages'
+import { Route, BrowserRouter } from "react-router-dom";
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
   return (
-    <div className= {s.app_wrapper}>
-      <Header />
-      <Navigation />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className='app_wrapper'>
+        <Header />
+        <Navigation />
+        <div className='app_wrapper_content'>
+          <Route path='/profile' component={Profile} />
+          <Route path='/messages' component={Messages} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import s from './Messages.module.css';
 
-const Messages =()=>{
+const Messages = () => {
     return <div className={s.messages}>
-        Messages
+        <div className={s.dialogueItems}>
+            <div className={s.dialogue + ' ' + s.active}>
+Igor Zaripov</div>
+            <div className={s.dialogue}>Roman Zaripov</div>
+            <div className={s.dialogue}>Nikita Kuznetsov</div>
+        </div>
+        <div className={s.messageItems}>
+            <div className={s.message}>Hi!</div>
+            <div className={s.message}>Hello</div>
+            <div className={s.message}>How are you?</div>
+        </div>
     </div>;
 }
 
