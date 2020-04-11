55ec165d4dd010b20e4cf1b49774462ec9f55aed
Kinzar97 <igor.zaripov.igor@gmail.com>
2020-04-12 02:15:53 +0300

    transferred data in index.js

--- src/App.js
+++ src/App.js
@@ -10,33 +10,15 @@ import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 
 
-let messages = [
-  { id: 1, message: 'Hello' },
-  { id: 2, message: 'Hello' },
-  { id: 3, message: 'How are you?' },
-]
-
-let dialogues = [
-  { id: 1, name: 'Igor Zaripov' },
-  { id: 2, name: 'Roman Zaripov' },
-  { id: 3, name: 'Nikita Kuznetsov' }
-]
-
-let posts = [
-  { id: 1, message: 'Hi! How are you?', likes: 2 },
-  { id: 2, message: 'YOOO', likes: 20 },
-  { id: 3, message: 'FIREEE', likes: 30 },
-]
-
-const App = () => {
+const App = (props) => {
   return (
     <BrowserRouter>
       <div className='app_wrapper'>
         <Header />
         <Navigation />
         <div className='app_wrapper_content'>
-          <Route path='/profile' render={() => <Profile posts={posts}/>} />
-          <Route path='/messages' render={() => <Messages messages={messages} dialogues={dialogues} />} />
+          <Route path='/profile' render={() => <Profile posts={props.posts}/>} />
+          <Route path='/messages' render={() => <Messages messages={props.messages} dialogues={props.dialogues} />} />
           <Route path='/news' component={News} />
           <Route path='/music' component={Music} />
           <Route path='/settings' component={Settings} />
--- src/index.js
+++ src/index.js
@@ -4,9 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let messages = [
+  { id: 1, message: 'Hello' },
+  { id: 2, message: 'Hello' },
+  { id: 3, message: 'How are you?' },
+]
+
+let dialogues = [
+  { id: 1, name: 'Igor Zaripov' },
+  { id: 2, name: 'Roman Zaripov' },
+  { id: 3, name: 'Nikita Kuznetsov' }
+]
+
+let posts = [
+  { id: 1, message: 'Hi! How are you?', likes: 2 },
+  { id: 2, message: 'YOOO', likes: 20 },
+  { id: 3, message: 'FIREEE', likes: 30 },
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App dialogues={dialogues} messages={messages} posts={posts} />
   </React.StrictMode>,
   document.getElementById('root')
 );
